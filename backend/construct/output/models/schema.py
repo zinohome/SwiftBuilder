4b2b9acceee42c006d0b4098af2e10eb8878c0a3
zhangjun <ibmzhangjun@139.com>
2024-02-24 22:21:24 +0800

    Init

---
+++ backend/construct/output/models/schema.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  #
+#  Copyright (C) 2023 ZinoHome, Inc. All Rights Reserved
+#  #
+#  @Time    : 2023
+#  @Author  : Zhang Jun
+#  @Email   : ibmzhangjun@139.com
+#  @Software: SwiftApp
+from datetime import date, datetime
+from decimal import Decimal
+from fastapi_amis_admin import models, amis
+from typing import Optional, List, TYPE_CHECKING
+
+from fastapi_amis_admin.models import Field
+from sqlalchemy import func
+from sqlmodel import Relationship
+from sqlmodelx import SQLModel
+
+from core import i18n as _
+
+class SwiftSQLModel(SQLModel):
+    class Config:
+        use_enum_values = True
+        orm_mode = True
+        arbitrary_types_allowed = True
+
+class Schema(SwiftSQLModel, table=True):
+    __tablename__ = 'schema'
+    schema_id: Optional[int] = models.Field(default=None,
+                                                    title='ID',
+                                                    primary_key=True,
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    applicaiton_id: Optional[int] = models.Field(default=None,
+                                                    title='应用ID',
+                                                    foreign_key='application.applicaiton_id',
+                                                    nullable=False,
+                                                    index=True,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    name: str = models.Field(default=None,
+                                                    title='导航项名称',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    group_schema: str = models.Field(default=None,
+                                                    title='导航组模式',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    label: str = models.Field(default=None,
+                                                    title='导航项标签',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    title: str = models.Field(default=None,
+                                                    title='导航项标题',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    icon: str = models.Field(default=None,
+                                                    title='导航项图标',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    sort: str = models.Field(default=None,
+                                                    title='导航项顺序',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    model: str = models.Field(default=None,
+                                                    title='对应模型',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    model_file: str = models.Field(default=None,
+                                                    title='模型文件',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    pk_name: str = models.Field(default=None,
+                                                    title='模型主键',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    list_per_page: str = models.Field(default=None,
+                                                    title='分页记录数',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    list_display: str = models.Field(default=None,
+                                                    title='页面显示列',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    search_fields: str = models.Field(default=None,
+                                                    title='搜索字段',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    parent_class: str = models.Field(default=None,
+                                                    title='上级页面类',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    print: str = models.Field(default=None,
+                                                    title='页面打印',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    enable_bulk_create: str = models.Field(default=None,
+                                                    title='启用批量新增',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    schema_read: str = models.Field(default=None,
+                                                    title='启用查看页',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    action_type: str = models.Field(default=None,
+                                                    title='表单弹出方式',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    sub_include: str = models.Field(default=None,
+                                                    title='子页面',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    create_time: datetime = models.Field(default_factory= datetime.now,
+                                                    title='Create Time',
+                                                    nullable=False,
+                                                    index=True,
+                                                    amis_form_item=amis.InputDatetime(disabled=True),
+                                                    amis_table_column = "")
+    update_time: Optional[datetime] = models.Field(default_factory= datetime.now,
+                                                    title='Update Time',
+                                                    nullable=False,
+                                                    index=True,
+                                                    sa_column_kwargs={"onupdate": func.now(), "server_default": func.now()},
+                                                    amis_form_item=amis.InputDatetime(disabled=True),
+                                                    amis_table_column = "")
---
+++ backend/construct/output/models/schemagroup.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  #
+#  Copyright (C) 2023 ZinoHome, Inc. All Rights Reserved
+#  #
+#  @Time    : 2023
+#  @Author  : Zhang Jun
+#  @Email   : ibmzhangjun@139.com
+#  @Software: SwiftApp
+from datetime import date, datetime
+from decimal import Decimal
+from fastapi_amis_admin import models, amis
+from typing import Optional, List, TYPE_CHECKING
+
+from fastapi_amis_admin.models import Field
+from sqlalchemy import func
+from sqlmodel import Relationship
+from sqlmodelx import SQLModel
+
+from core import i18n as _
+
+class SwiftSQLModel(SQLModel):
+    class Config:
+        use_enum_values = True
+        orm_mode = True
+        arbitrary_types_allowed = True
+
+class Schemagroup(SwiftSQLModel, table=True):
+    __tablename__ = 'schemagroup'
+    schemagroup_id: Optional[int] = models.Field(default=None,
+                                                    title='ID',
+                                                    primary_key=True,
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    applicaiton_id: Optional[int] = models.Field(default=None,
+                                                    title='应用ID',
+                                                    foreign_key='application.applicaiton_id',
+                                                    nullable=False,
+                                                    index=True,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    group_name: str = models.Field(default=None,
+                                                    title='导航组名称',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    group_schema: str = models.Field(default=None,
+                                                    title='导航组模式',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    label: str = models.Field(default=None,
+                                                    title='导航组标签',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    title: str = models.Field(default=None,
+                                                    title='导航组标题',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    icon: str = models.Field(default=None,
+                                                    title='导航组图标',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    sort: str = models.Field(default=None,
+                                                    title='导航组顺序',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    router_prefix: str = models.Field(default=None,
+                                                    title='导航组路径',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    create_time: datetime = models.Field(default_factory= datetime.now,
+                                                    title='Create Time',
+                                                    nullable=False,
+                                                    index=True,
+                                                    amis_form_item=amis.InputDatetime(disabled=True),
+                                                    amis_table_column = "")
+    update_time: Optional[datetime] = models.Field(default_factory= datetime.now,
+                                                    title='Update Time',
+                                                    nullable=False,
+                                                    index=True,
+                                                    sa_column_kwargs={"onupdate": func.now(), "server_default": func.now()},
+                                                    amis_form_item=amis.InputDatetime(disabled=True),
+                                                    amis_table_column = "")
