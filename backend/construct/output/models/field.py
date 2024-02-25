39b95b911655078f612c661e46a8dbfd69b3c104
zhangjun <ibmzhangjun@139.com>
2024-02-25 10:24:49 +0800

    Init

---
+++ backend/construct/output/models/field.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  #
+#  Copyright (C) 2023 ZinoHome, Inc. All Rights Reserved
+#  #
+#  @Time    : 2023
+#  @Author  : Zhang Jun
+#  @Email   : ibmzhangjun@139.com
+#  @Software: SwiftApp
+from datetime import date, datetime
+from decimal import Decimal
+from fastapi_amis_admin import models, amis
+from typing import Optional, List, TYPE_CHECKING
+
+from fastapi_amis_admin.models import Field
+from sqlalchemy import func
+from sqlmodel import Relationship
+from sqlmodelx import SQLModel
+
+from core import i18n as _
+
+class SwiftSQLModel(SQLModel):
+    class Config:
+        use_enum_values = True
+        orm_mode = True
+        arbitrary_types_allowed = True
+
+class Field(SwiftSQLModel, table=True):
+    __tablename__ = 'field'
+    field_id: Optional[int] = models.Field(default=None,
+                                                    title='ID',
+                                                    primary_key=True,
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    model_id: Optional[int] = models.Field(default=None,
+                                                    title='应用ID',
+                                                    foreign_key='model.model_id',
+                                                    nullable=False,
+                                                    index=True,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    name: str = models.Field(default=None,
+                                                    title='字段名称',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    title: str = models.Field(default=None,
+                                                    title='字段标题',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    type: str = models.Field(default=None,
+                                                    title='字段类型',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    optional: str = models.Field(default=None,
+                                                    title='可选类型',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    default_factory: str = models.Field(default=None,
+                                                    title='默认值工厂',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    default: str = models.Field(default=None,
+                                                    title='默认值',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    primary_key: str = models.Field(default=None,
+                                                    title='主键',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    foreign_key: str = models.Field(default=None,
+                                                    title='外键',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    nullable: str = models.Field(default=None,
+                                                    title='可否为空',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    index: str = models.Field(default=None,
+                                                    title='索引字段',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    sa_column_kwargs: str = models.Field(default=None,
+                                                    title='字段列构造',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    amis_form_item: str = models.Field(default=None,
+                                                    title='表单显示',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    amis_table_column: str = models.Field(default=None,
+                                                    title='列显示',
+                                                    nullable=False,
+                                                    index=False,
+                                                    amis_form_item = "",
+                                                    amis_table_column = "")
+    create_time: datetime = models.Field(default_factory= datetime.now,
+                                                    title='Create Time',
+                                                    nullable=False,
+                                                    index=True,
+                                                    amis_form_item=amis.InputDatetime(disabled=True),
+                                                    amis_table_column = "")
+    update_time: Optional[datetime] = models.Field(default_factory= datetime.now,
+                                                    title='Update Time',
+                                                    nullable=False,
+                                                    index=True,
+                                                    sa_column_kwargs={"onupdate": func.now(), "server_default": func.now()},
+                                                    amis_form_item=amis.InputDatetime(disabled=True),
+                                                    amis_table_column = "")
